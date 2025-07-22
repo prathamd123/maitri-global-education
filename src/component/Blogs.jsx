8f4075324c425264237b4211b03d5406a3a2adf9
Mahesh <maheshmetre2005@gmail.com>
2025-07-22 22:20:46 +0530

    footer done

--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Blogs() {
   return (
-    <div className='bg-black text-white p-8'>
-        <h1 className='text-6xl font-cinzel font text-[#EAB308] '>BLOGS</h1>
+    <div className='bg-black text-white p-8 flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-6xl font-cinzel text-[#EAB308] '>BLOGS</h1>
         <button className='bg-[brown] font-cinzel text-xl text-white font-bold p-2 rounded-lg'>All Blogs</button>
     </div>
   )
